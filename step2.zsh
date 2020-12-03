237cae426641725748e12cc1fc584393a2acd8d5
mingzailao <mingzailao@126.com>
2020-12-03 09:03:31 +0000

    add

--- step2.zsh
+++ step2.zsh
@@ -1,4 +1,4 @@
 cp ./.zshrc ~/
 p10k reload ./.p10k.zsh
 cp myexport.zsh ~/
-echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
+# echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
