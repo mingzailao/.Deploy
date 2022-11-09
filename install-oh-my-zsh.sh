07e997be33a874352712ed07ee45bdd0f414b16a
mingzailao <mingzailao@126.com>
2022-11-09 17:18:47 +0800

    sd

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-apt install zsh git wget -y
+sudo apt install zsh git wget -y
 git config --global user.email "mingzailao@126.com"
 git config --global user.name "mingzailao"
-usermod -s /usr/bin/zsh $(whoami)
-sh ./install-from-git.sh
+sudo usermod -s /usr/bin/zsh $(whoami)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 cp ./.zshrc ~/
 cp ./.p10k.zsh ~/
 cp  .myexport.zsh ~/
